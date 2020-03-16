6a87d92cd9c00c58e71d4a9a5f4a52514e6bea6a
FiredBread <958265313@qq.com>
2020-03-17 00:48:44 +0800

    config project

---
+++ 01/twosum.cpp
@@ -0,0 +1,21 @@
+#include <headdll.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+    }
+};
+
+int main()
+{
+    string s;
+    cin >>s;
+    
+    cout << 111 << endl;
+    cout<<"good"<<endl;
+    while(1);
+    return 0;
+}
